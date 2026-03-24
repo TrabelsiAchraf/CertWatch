d7ca41658b331ca03aa034ee9219adbe36ef3c26
Achraf Trabelsi <a.trabelsi@oodrive.com>
2026-03-24 03:24:54 +0100

    Initial Commit

---
+++ CertWatch/CertWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertWatchApp.swift
+//  CertWatch
+//
+//  Created by Achraf Trabelsi on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CertWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
